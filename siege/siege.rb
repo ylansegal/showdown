c398c64e66dc91a79e168fb3e3ccde8cd61de78a
Ylan Segal <ylan@segal-family.com>
2013-05-05 20:47:45 -0700

    Add siege script and results

---
+++ siege/siege.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+url = 'https://unicorn-showdown.herokuapp.com/articles'
+server = 'unicorn'
+users = [1, 2, 4, 8, 16, 32, 64, 128]
+
+puts "Warming up..."
+`siege -c1 -t10s #{url}`
+
+users.each do |user_count|
+  puts "Runnning test with #{user_count} users"
+  `siege -b -c#{user_count} -t30s #{url} --quiet &> siege/#{server}/#{user_count}.txt`
+end
