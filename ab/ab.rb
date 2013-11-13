3605f47d3063c6d3853066dc5fc2f82bf4a36c7c
Ylan Segal <ylan@segal-family.com>
2013-11-13 15:10:10 -0800

    Add results using ab

---
+++ ab/ab.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+url = 'http://127.0.0.1:5000/articles'
+server = 'unicorn'
+users = [1, 2, 4, 8, 16, 32, 64, 128]
+
+puts "Warming up..."
+`ab #{url}`
+
+users.each do |user_count|
+  puts "Runnning test with #{user_count} users"
+  `ab -c #{user_count} -t 30 #{url}  &> ab/#{server}/#{user_count}.txt`
+end
